dca12542e715c5d946895a3db215ba0d5560958d
mosehmaguah <you@example.com>
2023-12-07 03:37:14 +0300

    files_manager project

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-import express from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
-export default function Routes(app) {
-  const router = express.Router();
-  app.use('/', router);
+const router = Router();
 
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+router.get('/status', AppController.getStatus);
 
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+router.get('/stats', AppController.getStats);
 
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
-}
+router.post('/users', UsersController.postNew);
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
+
+module.exports = router;
